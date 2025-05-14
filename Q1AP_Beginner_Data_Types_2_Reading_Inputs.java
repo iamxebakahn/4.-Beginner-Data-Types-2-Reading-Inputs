9388d44ff31eb60ca73503c671bef03b29f54d10
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:55:09 +0530

    Add files via upload

---
+++ Q1AP_Beginner_Data_Types_2_Reading_Inputs.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q1AP_Beginner_Data_Types_2_Reading_Inputs{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.print(a+b+c);
+    }
+}
