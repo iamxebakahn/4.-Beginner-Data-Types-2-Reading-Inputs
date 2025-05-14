be46cb588835e44d65be7230d6a74f450df7d24e
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:51:48 +0530

    Update Q1_Beginner_Data_Types_2_Reading_Inputs.java

--- Q1_Beginner_Data_Types_2_Reading_Inputs.java
+++ Q1_Beginner_Data_Types_2_Reading_Inputs.java
@@ -1,3 +1,33 @@
+// Problem Description
+
+// You're going to write some code to help you cook a gorgeous lasagna from your favorite cookbook. According to your cookbook, the Lasagna should be in the oven for 40 minutes. Given the time (in minutes), the lasagna has been in the oven, find how many more minutes the lasagna still needs to bake for.
+
+
+// Problem Constraints
+
+// 0 <= N <= 40
+
+
+// Input Format
+
+// The only first line contains the integer N, denoting the actual time (in minutes) the lasagna has been in the oven for.
+
+
+// Output Format
+
+// Print in a single line how many minutes the lasagna needs to bake.
+
+
+// Example Input
+
+// Input: 
+// 30
+
+
+// Example Output
+
+// Output: 
+// 10
 import java.lang.*;
 import java.util.*;
 
