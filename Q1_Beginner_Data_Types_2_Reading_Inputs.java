6e4a73813ba377f39994677ff425ceb277e9a08c
iamxebakahn <zebadwight@gmail.com>
2025-05-14 19:51:18 +0530

    Add files via upload

---
+++ Q1_Beginner_Data_Types_2_Reading_Inputs.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+public class Q1_Beginner_Data_Types_2_Reading_Inputs{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);
+        int time= sc.nextInt();
+        System.out.print(40-time);
+    }
+}   
